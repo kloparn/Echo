06c1e3911ff90b13ec3ae6fe0aac39f3ec366505
kloparn <adam97.live.com@live.com>
2021-09-23 21:58:41 +0200

    Functionally update
    - Add pp command to list multiple videos.
    
    - Update playSound function to also display the current playing title + link.
    
    - Add new helper that takes out multiple videos at once.
    
    - Update queue command to also display index.

--- src/commands/queue.ts
+++ src/commands/queue.ts
@@ -3,5 +3,6 @@ const globalData = ClientMemory.getInstance();
 
 export = () => {
   if (!globalData.isConnectedToVoice) return globalData.channel.send("im not connected");
-  return globalData.channel.send(`current queue: ${globalData.queue.join("\n")}`)
+  const queueArray = globalData.queue.map((item, index) => `${index + 1} | ${item}`);
+  return globalData.channel.send(`current queue: \n${queueArray.join("\n")}`)
 };
