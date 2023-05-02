713666d32fd72eb033424da838f1a5e44c4c1bb4
Adam hakansson <adam97.live.com@live.com>
2023-05-02 19:57:50 +0200

    whitelist for queue add

--- src/helpers/searchVideo.ts
+++ src/helpers/searchVideo.ts
@@ -1,6 +1,8 @@
 import yts from "yt-search";
+import getValidVideoUrl from "./getValidVideoUrl";
 
 const searchVideo = async (searchTerm: string) => {
+  searchTerm = getValidVideoUrl(searchTerm);
   const response = await yts(searchTerm);
 
   const videos = response.all.filter((it: any) => it.type === "video");
