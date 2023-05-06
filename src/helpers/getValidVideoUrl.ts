9f19eafa9ad9593296e3acf5a3121a4bb4b5710e
Adam hakansson <adam97.live.com@live.com>
2023-05-06 15:53:42 +0200

    Fix getValidVideoUrl issue

--- src/helpers/getValidVideoUrl.ts
+++ src/helpers/getValidVideoUrl.ts
@@ -1,5 +1,7 @@
 export default (url) => {
   const u = new URL(url);
 
-  return `${u.origin}${u.pathname}?v=${u.searchParams.get("v")}`
- }
+  if (!u.searchParams.get("v")) return url;
+
+  return `${u.origin}${u.pathname}?v=${u.searchParams.get("v")}`;
+};
