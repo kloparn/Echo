1cf39c7a859f2285c26a66c4dbb7795f860d6677
Adam hakansson <adam97.live.com@live.com>
2023-08-02 15:26:31 +0200

    Remove not used code and remove check for another bot on wipe

--- src/commands/wipe.ts
+++ src/commands/wipe.ts
@@ -1,19 +1,18 @@
-import { CommandInteraction, SlashCommandBuilder, User } from "discord.js";
-import ClientMemory from "../classes/ClientMemory";
-import clientHandler from "../helpers/clientHandler";
-import { deleteReply } from "../helpers/messageHelper";
+import { CommandInteraction, SlashCommandBuilder } from "discord.js";
 import { Command } from "../interfaces";
-
-const globalData = ClientMemory.getInstance();
 
 const execute = async (interaction: CommandInteraction) => {
   const messageManager = interaction.channel.messages;
 
   await interaction.reply("Started wiping the channel history");
 
-  const messages = await messageManager.fetch({ limit: 100 });
+  let messages = await messageManager.fetch({ limit: 100 });
 
-  messages.filter((m) => m.author.bot || m.author.id === "184405311681986560");
+  if (messages.values.length === 1) {
+    messages = await messageManager.fetch({ limit: 100 });
+  }
+
+  messages.filter((m) => m.author.bot);
 
   let counter = 0;
 
