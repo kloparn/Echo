142f91937e0bff2c6fba43adfede7b83030afaef
Adam hakansson <adam97.live.com@live.com>
2023-09-23 10:41:26 +0200

    Add condition to message deletion

--- src/helpers/messageHelper.ts
+++ src/helpers/messageHelper.ts
@@ -1,8 +1,13 @@
 import { ButtonInteraction, CommandInteraction } from "discord.js";
 
+let justDeletedId;
+
 const deleteReply = (interaction: ButtonInteraction | CommandInteraction, timeBeforeDeletion: number) => {
   setTimeout(async () => {
-    await interaction.deleteReply();
+    if (justDeletedId !== interaction.id) {
+      await interaction.deleteReply();
+      justDeletedId = interaction.id;
+    }
   }, timeBeforeDeletion);
 };
 
